0b055e7e80badc5066270d39d1794cea1bb698d1
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-06-27 02:17:04 -0500

    added more route and controller for delete and patch

--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const taskController = require("./tasks.controller.js")
+const taskController = require("./tasks.controller.js");
 const tasksRouter = express.Router();
 
 tasksRouter.get("/tasks", taskController.handleGetTasks);
 
-tasksRouter.post("/tasks", (req, res) => {
-  console.log(req.body);
-  return res.send("Create a new Task");
-});
+tasksRouter.post("/tasks", taskController.handlePostTasks);
+
+tasksRouter.patch("/tasks", taskController.handlePatchTasks);
+
+tasksRouter.delete("/tasks", taskController.handleDeleteTasks);
 
 module.exports = tasksRouter;
