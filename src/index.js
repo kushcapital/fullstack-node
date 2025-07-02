7c8e34a5b698ae969285e9b19ee49a9ddcd361e5
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-07-02 00:28:23 -0500

    added how to resolve when endpoint link is different than intended error 404

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const responseFormatter = require("./middleware/responseFormatter.js");
+const {StatusCodes} = require("http-status-codes")
 
 const app = express();
 const port = 3001; //http://localhost:3001/
@@ -31,6 +32,10 @@ app.use(morgan("combined", { stream: accessLogStream }));
 app.use(responseFormatter); //note the middleware should be above to route inorder to middlware to process before sending the response.
 app.use("/", tasksRouter);
 
+
+app.use((req,res)=>{
+  res.status(StatusCodes.NOT_FOUND).json(null)
+})
 //listening to the port 3001
 app.listen(port, () => {
   console.log(`App listening on the port no: ${port}`);
