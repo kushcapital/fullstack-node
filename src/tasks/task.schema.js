e6278b29c9eb63accbf7b5a7c4b97ef44a6363ac
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-07-07 22:19:08 -0500

    Creating the First Schema

---
+++ src/tasks/task.schema.js
@@ -0,0 +1,36 @@
+const { Schema } = require("mongoose");
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Task title is required"],
+    trim: true,
+    maxLength: [100, "Title cannot be more than 100 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Task title is required"],
+    trim: true,
+    maxLength: [500, "Title cannot be more than 500 characters"],
+  },
+  status: {
+    type: String,
+    required: [true, "Task status is required"],
+    enum: ["todo", "inProgress", "completed"],
+    default: "todo",
+  },
+  priority: {
+    type: String,
+    required: [true, "Task priority is required"],
+    enum: ["low", "normal", "high"],
+    default: "normal",
+  },
+  dueDate: {
+    type: Date,
+    required: [true, "Task due date is required"],
+  },
+  createdAt: {
+    type: Date,
+    dafault: Date.now,
+  },
+});
