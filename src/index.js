d02fec1b0116fe3b37d1fb8722c3a08495309318
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-07-07 21:57:39 -0500

    intergating Mongoose with express

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const responseFormatter = require("./middleware/responseFormatter.js");
-const {StatusCodes} = require("http-status-codes")
-const authRouter = require("./auth/auth.router.js")
-const createUser = require("./users/users.router.js")
+const { StatusCodes } = require("http-status-codes");
+const authRouter = require("./auth/auth.router.js");
+const createUser = require("./users/users.router.js");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3001; //http://localhost:3001/
@@ -36,11 +37,25 @@ app.use("/", tasksRouter);
 app.use("/auth", authRouter);
 app.use("/users", createUser);
 
-
-app.use((req,res)=>{
-  res.status(StatusCodes.NOT_FOUND).json(null)
-})
-//listening to the port 3001
-app.listen(port, () => {
-  console.log(`App listening on the port no: ${port}`);
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json(null);
 });
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://kushwaharajeev:%40Aryan12345@nodejs.exgbf6f.mongodb.net/",
+      { dbname: "fullstackTasks" }
+    );
+    console.log("Connected to MongoDB");
+    //listening to the port 3001
+    app.listen(port, () => {
+      console.log(`App listening on the port no: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
