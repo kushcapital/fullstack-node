72e5f9fb8ec927164fad2baa7082a49e38e143ac
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-07-10 01:23:35 -0500

    Create seperate validator files for post task

--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -3,28 +3,18 @@ const { body, validationResult } = require("express-validator");
 const taskController = require("./tasks.controller.js");
 const { StatusCodes } = require("http-status-codes");
 const tasksRouter = express.Router();
+const createTaskValidator = require("./validators/createTask.validator.js");
 
 tasksRouter.get("/tasks", taskController.handleGetTasks);
 
-tasksRouter.post(
-  "/tasks",
-  [
-    body("title", "This title cannot be empty").notEmpty(),
-    body("title", "This title must be String").isString(),
-    body("dueDate", "dueDate needs to be a valid ISO8601 string")
-      .notEmpty()
-      .isISO8601(),
-  ],
-  (req, res) => {
-    const result = validationResult(req);
-    // console.log(result);
-    if (result.isEmpty()) {
-      return taskController.handlePostTasks(req,res);
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).json(result.array());
-    }
+tasksRouter.post("/tasks", createTaskValidator, (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return taskController.handlePostTasks(req, res);
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json(result.array());
   }
-);
+});
 
 tasksRouter.patch("/tasks", taskController.handlePatchTasks);
 
---
+++ src/tasks/validators/createTask.validator.js
@@ -0,0 +1,23 @@
+
+const { body } = require("express-validator");
+
+const createTaskValidator = [
+  body("title", "This title cannot be empty").notEmpty(),
+  body("title", "This title must be String").isString(),
+  body("title").isLength({ max: 100 }),
+  body("title").trim(),
+  body("dueDate", "dueDate needs to be a valid ISO8601 string")
+    .notEmpty()
+    .isISO8601(),
+  body("description", "the description cannot be empty and needs to be string")
+    .notEmpty()
+    .isString()
+    .trim(),
+  body("description", "the description cannot be more then 500 chars").isLength(
+    { max: 500 }
+  ),
+  body("priority").isIn(["low", "normal", "high"]),
+  body("status").isIn(["todo", "inProgress", "completed"]),
+];
+
+module.exports = createTaskValidator;
