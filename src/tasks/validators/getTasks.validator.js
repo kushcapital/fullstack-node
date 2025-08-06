dc559c7f53ae0d5910a9de03956f5375f20c23e2
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-05 21:33:21 -0500

    adding default setting for pagination where param is not provide. we also use custom sanitizor in validator.

--- src/tasks/validators/getTasks.validator.js
+++ src/tasks/validators/getTasks.validator.js
@@ -1,11 +1,23 @@
 const { query } = require("express-validator");
 
 const getTaskValidator = [
-  query("limit", "limit must be valid int").optional().isInt().toInt(),
+  query("limit", "limit must be valid int")
+    .optional()
+    .isInt()
+    .toInt({ min: 1 }),
+  query("limit").customSanitizer((value, { req }) => {
+    return value ? value : 5;
+  }),
   query("page", "page must be valid int").optional().isInt().toInt(),
+  query("page").customSanitizer((value, { req }) => {
+    return value ? value : 1;
+  }),
   query("order", "order must be one of ['asc', 'dsc'];")
     .optional()
     .isIn(["asc", "dsc"]),
+  query("order").customSanitizer((value, { req }) => {
+    return value ? value : "asc";
+  }),
 ];
 
 module.exports = getTaskValidator;
