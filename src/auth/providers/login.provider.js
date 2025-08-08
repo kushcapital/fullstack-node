8547772433a06b8a565f5a4a171b43ae84d7e0f8
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-08 17:09:01 -0500

    feature generating Json web token JWT

--- src/auth/providers/login.provider.js
+++ src/auth/providers/login.provider.js
@@ -3,33 +3,41 @@ const { StatusCodes } = require("http-status-codes");
 const errorLogger = require("../../helpers/errorLogger.helper.js");
 const User = require("../../users/user.schema.js");
 const bcrypt = require("bcrypt");
+const generateTokenProvider = require("./generateTokenProvider.js");
 
 async function loginProvider(req, res) {
   const validatedData = matchedData(req);
   try {
     // Find user by email
     const user = await User.findOne({ email: validatedData.email });
-    
+
     if (!user) {
       return res.status(StatusCodes.UNAUTHORIZED).json({
         reason: "Invalid email or password",
       });
     }
-    
+
     // Check password
-    const isPasswordValid = await bcrypt.compare(validatedData.password, user.password);
-    
+    const isPasswordValid = await bcrypt.compare(
+      validatedData.password,
+      user.password
+    );
+
     if (!isPasswordValid) {
       return res.status(StatusCodes.UNAUTHORIZED).json({
         reason: "Invalid email or password",
       });
     }
-    
+
+    //generating Jwt for user
+    const token = generateTokenProvider(user);
+
     // Login successful - return user info (excluding password)
     const { password, ...userInfo } = user.toObject();
     return res.status(StatusCodes.OK).json({
       message: "Login successful",
       user: userInfo,
+      accessToken: token,
     });
   } catch (error) {
     errorLogger("Error while trying to login", req, error);
